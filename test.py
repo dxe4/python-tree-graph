b37b49a9b5dfe5a50a6378afc6c88a0c9bcec598
charalambosp <papaloizouc@gmail.com>
2014-03-15 10:31:09 +0000

    refactor

--- test.py
+++ test.py
@@ -17,25 +17,29 @@ names = ["Sophia", "Emma", "Olivia", "Isabella", "Ava", "Lily", "Zoe", "Chloe", 
          "Reese", "Clara", "Paige", "Kate", "Nevaeh", "Sadie", "Quinn", "Isla",
 ]
 
-def chunks(size):
-    name_size = len(names)
-    assert name_size % size == 0
-    list_size = name_size / size
-    temp = [(int((i-1)*list_size), int(list_size*i)) for i in range(1, size+1)]
-    return [names[i[0]:i[1]] for i in temp]
+
+def chunks(size, _list):
+    list_size = len(_list)
+    assert list_size % size == 0
+    new_list_size = list_size / size
+    temp = [(int((i - 1) * new_list_size), int(new_list_size * i))
+            for i in range(1, size + 1)]
+    return [_list[i[0]:i[1]] for i in temp]
 
 
 class TestAdding(unittest.TestCase):
     def setUp(self):
         graph = Graph()
+        nodes = self.make_nodes()
 
     def make_nodes(self):
-        pass
+        _chunks = chunks(5, names)
+        node_list = lambda _list: [Node(j) for j in _list]
+        return [node_list(i) for i in _chunks]
 
     def test_board_init(self):
         pass
 
 
 if __name__ == '__main__':
-    chunks(5)
     unittest.main()
